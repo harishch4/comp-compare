12d324dbafd5ff28c36261f63503156b839be713
Harish <harish.ch@morphing.in>
2022-03-19 13:02:16 +0530

    processors added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from email import contentmanager
 from email.policy import default
 import imp
+from dump import processor, processorEngine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 
 from flask import Flask, redirect,render_template, request, url_for
@@ -9,32 +12,22 @@ from datetime import date, datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///processor.db'
 
 
-@app.route('/', methods = ['POST','GET'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue!"
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+    try:
+        # create session and add objects
+        with Session(processorEngine) as session:
+            statement = select(processor)
+            processors = session.execute(statement).all()  
+            for row in processors:
+                print(row.processorName)
+            return render_template('index.html', processors=processors)
+    except Exception as e: print(e)
+    return "Somthing wrong!"
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
